5943562de05084fb795934a3f15659fac3ef79fd
huangmengyao <2594838054@qq.com>
2023-07-29 23:44:41 +0800

    first commit

---
+++ src/components/MyList.vue
@@ -0,0 +1,43 @@
+<template>
+    <ul class="todo-main">
+      <MyItem v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj"/>
+    </ul>
+</template>
+
+<script>
+import MyItem from "./MyItem.vue";
+export default {
+  // name: "MyList",
+  components: {
+    MyItem,
+  },
+  data(){
+    return {
+      todos:[
+        {id:'001', name:'吃饭', done: true},
+        {id:'002', name:'喝酒', done: true},
+        {id:'003', name:'开车', done: true},
+      ]
+    }
+  }
+};
+</script>
+
+<style>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+// 该文件是整个项目的入口文件
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 将APP组件放入容器中
   render: h => h(App),
 }).$mount('#app')
+// 上面这个相当于el:'#app'
