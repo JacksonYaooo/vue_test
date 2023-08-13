f197abdb436e2ee65ef4afb2411cc8ef65b4b2fa
huangmengyao <2594838054@qq.com>
2023-08-13 16:46:41 +0800

    8/13

--- 21_src_github搜索案例_vue-resource/main.js
+++ 21_src_github搜索案例_vue-resource/main.js
@@ -3,20 +3,17 @@ import Vue from 'vue'
 //引入App
 import App from './App.vue'
 //引入插件
-// import vueResource from 'vue-resource'
-// 引入store
-import store from './store/index'
+import vueResource from 'vue-resource'
 //关闭Vue的生产提示
 Vue.config.productionTip = false
 //使用插件
-// Vue.use(vueResource)
+Vue.use(vueResource)
 
 //创建vm
 new Vue({
 	el:'#app',
 	render: h => h(App),
-	store,
 	beforeCreate() {
 		Vue.prototype.$bus = this
-	}
+	},
 })
